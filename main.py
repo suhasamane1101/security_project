4546364ef186bf588ef78dddef8832ff68cf87d8
suhasamane1101 <suhasamane2@gmail.com>
2025-10-16 23:15:09 +0530

    mlflow technique added

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ from networksecurity.entity.config_entity import TrainingPipelineConfig
 
 import sys
 
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.entity.config_entity import ModelTrainerConfig
+
+
 if __name__=='__main__':
     try:
         trainingpipelineconfig=TrainingPipelineConfig()
@@ -29,6 +33,13 @@ if __name__=='__main__':
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info("data transformation completed")
+
+        logging.info("Model Training started")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
  
     except Exception as e:
            raise NetworkSecurityException(e,sys)
